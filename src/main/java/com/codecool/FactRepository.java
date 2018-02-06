f63e6ad91888acde0b25d35033a86191dd7c2456
GaborMartin <gabormartin94@gmail.com>
2018-02-06 14:16:01 +0100

    Implemented basic FactRepository class, added getters to FactRep and RuleRep classes

--- src/main/java/com/codecool/FactRepository.java
+++ src/main/java/com/codecool/FactRepository.java
@@ -1,4 +1,19 @@
 package com.codecool;
 
+import java.util.ArrayList;
+
 public class FactRepository {
+    private ArrayList<Question> facts;
+
+    public ArrayList<Question> getFacts() {
+        return facts;
+    }
+
+    public FactRepository() {
+        facts = new ArrayList<>();
+    }
+
+    public void addFact(Fact fact) {
+        facts.add(fact);
+    }
 }
