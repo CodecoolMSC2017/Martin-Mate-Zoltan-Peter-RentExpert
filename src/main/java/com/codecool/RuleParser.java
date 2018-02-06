eb2d9768fde1bc2ab06fce917a2806349af1a992
peti120 <peti59550@gmail.com>
2018-02-06 10:33:28 +0100

    Base structure implemented

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -1,4 +1,36 @@
 package com.codecool;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class RuleParser extends XMLParser {
+
+    private RuleRepository ruleRepository;
+
+    public RuleParser(String rulesXml) throws ParserConfigurationException, SAXException, IOException {
+        loadXMLDocument(rulesXml);
+        this.ruleRepository = new RuleRepository();
+        for (Element e : getElements()) {
+           // this.ruleRepository.addQuestion(toQuestion(e));
+        }
+    }
+
+    private Element[] getElements() {
+        dom.getElementsByTagName("Rule");
+        return new Element[0];
+    }
+
+    private Question toQuestion(Element e) {
+        return null;
+    }
+
+    public RuleRepository getRuleRepository() {
+        return ruleRepository;
+    }
 }
--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -2,28 +2,27 @@ package com.codecool;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 import java.io.File;
+
+import java.io.IOException;
 
 public abstract class XMLParser {
 
-    public Document loadXMLDocument(String filename) {
+    private static final DocumentBuilderFactory DOCUMENT_BUILDER_FACTORY = DocumentBuilderFactory.newInstance();
 
-        try {
+    protected Document dom;
+    public void loadXMLDocument(String filename) throws IOException, SAXException, ParserConfigurationException {
+        File fXmlFile = new File(filename + ".xml");
+        DocumentBuilder dBuilder = DOCUMENT_BUILDER_FACTORY.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
 
-            File fXmlFile = new File(filename + ".xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
+        doc.getDocumentElement().normalize();
 
-            doc.getDocumentElement().normalize();
-
-            return doc;
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        return null;
+        dom = doc;
     }
 }
