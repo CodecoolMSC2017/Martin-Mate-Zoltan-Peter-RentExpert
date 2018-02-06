8c6d5ab9743481dc1968b0a244761db366cb58d2
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-06 13:43:40 +0100

    answerToQuestion filed is added, getEvalutedAnswer() method is implemented

--- src/main/java/com/codecool/Question.java
+++ src/main/java/com/codecool/Question.java
@@ -1,9 +1,12 @@
 package com.codecool;
 
+import java.util.HashMap;
+
 public class Question {
     private String id;
     private String question;
     private Answer answer;
+    private HashMap<String, Boolean> answerToQuestion;
 
     public Question(String id, String question, Answer answer) {
         this.id = id;
@@ -35,15 +38,23 @@ public class Question {
         this.answer = answer;
     }
 
-    public boolean getEvalutedAnswer(String input) {
-        int answers = answer.getValues().size();
+    public boolean getEvalutedAnswer(String input) { // függvényhívásnál 3-4 bekérés (counter számláló változó segítségével), ha sikertelen a próbálkozás akkor InvalidOperationException
         if (answer.evaluateAnswerByInput(input)) {
-            for (int i = 0; i < answers) {
-                if (answers[i] instanceof SingleValue) {
-
+            for (Value value: answer.getValues()) {
+                if (value instanceof SingleValue) {
+                    if (value.getInputPattern().get(0).equals(input))
+                        answerToQuestion.put(id, value.getSelectionType());
+                        return value.getSelectionType();
+                } else {
+                    for (int i = 0; i < value.getInputPattern().size(); i++) {
+                        if (input.equals(value.getInputPattern().get(i))) {
+                            answerToQuestion.put(id, value.getSelectionType());
+                            return value.getSelectionType();
+                        }
+                    }
                 }
             }
         }
-
+        return false;
     }
 }
