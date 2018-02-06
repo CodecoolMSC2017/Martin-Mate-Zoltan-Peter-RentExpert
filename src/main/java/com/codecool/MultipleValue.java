ee17009df59bddb8ece4a0cf09a6b0c9096571ad
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-06 13:44:23 +0100

    constructor is modified

--- src/main/java/com/codecool/MultipleValue.java
+++ src/main/java/com/codecool/MultipleValue.java
@@ -6,8 +6,9 @@ public class MultipleValue extends Value {
     private List<String> params;
     private boolean selectionType;
 
-    public MultipleValue(List<String> params) {
+    public MultipleValue(List<String> params, Boolean selectionType) {
         this.params = params;
+        this.selectionType = selectionType;
     }
 
     public List<String> getInputPattern() {
