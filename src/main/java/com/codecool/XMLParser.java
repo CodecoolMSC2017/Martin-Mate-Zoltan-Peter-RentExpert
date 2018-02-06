d8431ccbe2b92b43d61a1ba671a387b04d35f68e
Zoltan Kertesz <zoli.kertesz93@gmail.com>
2018-02-06 09:34:06 +0100

    xml parser: read() method implemented

--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -1,4 +1,29 @@
 package com.codecool;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import java.io.File;
+
 public abstract class XMLParser {
+
+    public Document reader(String filename) {
+
+        try {
+
+            File fXmlFile = new File(filename + ".xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            return doc;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
 }
