ff0bb2641be6c7e56245032c116b6b5acbd5d188
GaborMartin <gabormartin94@gmail.com>
2018-02-07 11:30:22 +0100

    Implemented fields and constructor in class ESProvider

--- src/main/java/com/codecool/ESProvider.java
+++ src/main/java/com/codecool/ESProvider.java
@@ -1,4 +1,23 @@
 package com.codecool;
 
+import javafx.css.Rule;
+
+import java.util.List;
+
 public class ESProvider {
+    private RuleRepository ruleRepository;
+    private FactRepository factRepository;
+    private List<Question> questions;
+    private List<Fact> facts;
+    private QuestionIterator qi;
+    private FactIterator fi;
+
+    public ESProvider(RuleParser ruleParser, FactParser factParser) {
+        this.ruleRepository = ruleParser.getRuleRepository();
+        this.factRepository = factParser.getFactRepository();
+        this.questions = ruleRepository.getQuestions();
+        this.facts = factRepository.getFacts();
+        this.qi = new QuestionIterator(questions);
+        this.fi = new FactIterator(facts);
+    }
 }
