22e4763119a619fa2997aa3ec2118aa0ecfdb053
GaborMartin <gabormartin94@gmail.com>
2018-02-07 10:50:42 +0100

    Implemented setFactValue method

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -1,7 +1,6 @@
 package com.codecool;
 
-import java.util.HashMap;
-import java.util.Set;
+import java.util.*;
 
 public class Fact {
 
@@ -20,7 +19,14 @@ public class Fact {
     }
 
     public void setFactValue(String id, boolean value) {
-
+        Set<String> evalsKeys = evals.keySet();
+        List<String> keysList = new ArrayList<>(evalsKeys);
+        Boolean evalValue = getValueById(id);
+        for (int i = 0; i < keysList.size(); i++) {
+            if (id.equals(keysList.get(i))) {
+                evalValue = value;
+            }
+        }
     }
 
     public boolean getValueById(String id) {
