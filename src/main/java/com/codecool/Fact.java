16333fc50743ed8d0a1a1ab3fd6e0578e041e6de
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-07 10:32:19 +0100

    fields is added, consturctor is implemented, getIdSet() method is implemented, getValueById() method is implemented, getDescription() method is implemented

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -1,4 +1,34 @@
 package com.codecool;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Fact {
+
+    private String id;
+    private String description;
+    private HashMap<String, Boolean> evals;
+
+    public Fact(String id, String description, HashMap<String, Boolean> evals) {
+        this.id = id;
+        this.description = description;
+        this.evals = evals;
+    }
+
+    public Set<String> getIdSet() {
+        return evals.keySet();
+    }
+
+    public void setFactValue(String id, boolean value) {
+
+    }
+
+    public boolean getValueById(String id) {
+        return evals.get(id);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
